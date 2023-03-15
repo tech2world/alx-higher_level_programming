23fe24374727bc43cd27f0bd91beb2cbb12776de
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-03-15 17:56:50 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+
+if __name__ == "__main__":
+    """Print addition of args"""
+    import sys
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+    print("{:d}".format(add))
