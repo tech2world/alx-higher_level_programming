8e7d19a9ace2fbf1dbf7a29fd19d5de1d917d3da
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-04-02 12:05:27 +0100

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A Square module"""
+
+
+class Square:
+    """ Defines a square"""
+    def __int__(self, size):
+        """ Initializing the square
+        Args:
+            size - represents the size of the square
+        """
+        self.__size = size
