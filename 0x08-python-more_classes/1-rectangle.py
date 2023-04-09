3a5625b2d2291530fab8628d339b996d827fe375
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-04-10 00:27:06 +0100

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
             ValueError: if size < 0
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -28,7 +28,7 @@ class Rectangle:
         """Sets width's attribute"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -42,7 +42,7 @@ class Rectangle:
         """Sets height's attribute"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         # noinspection PyAttributeOutsideInit
         self.__height = value
