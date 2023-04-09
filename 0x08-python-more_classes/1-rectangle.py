4de7fe3a575027e73a6b0c3e7738f8875c0c6e4c
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-04-10 00:22:08 +0100

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
             ValueError: if size < 0
         """
 
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -44,4 +44,5 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
+        # noinspection PyAttributeOutsideInit
         self.__height = value
