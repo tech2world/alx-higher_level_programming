976a1f84df02f420cc369a70badc55d892bc21c2
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-04-13 06:32:57 +0100

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""MOdule taht writes obj to text file using JSON representattion"""
+"""Module that writes obj to text file using JSON representation"""
 
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Writes obj to text using json representation"""
+    """Writes obj to text using json representation
+    Args:
+        my_obj: Object to be serialized
+        filename: name of the file where string is stored
+    """
 
     with open(filename, mode='w') as js_file:
         json.dump(my_obj, js_file)
