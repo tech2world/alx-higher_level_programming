02683451c03f9a9031046c8868137f795378ca34
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-04-15 23:08:11 +0100

    Create 12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Pascal Triangle Module"""
+
+
+# def pascal_triangle(n):
+#     """Defines a pascal triangle of n-size"""
+#
+#     if n <= 0:
+#         return []
+#
+#     
