bd6b8de004c957cb4a8c3ae9a0437377bd12aa7a
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-05-06 18:21:54 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4)
-    for n in sorted(names):
+    names = sorted(dir(hidden_4))
+    for n in names:
         if n[:2] != "__":
             print(names)
