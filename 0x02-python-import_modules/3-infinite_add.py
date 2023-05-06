cd206f006be30116c125c166da3ef069d9b6504c
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-05-06 17:39:41 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,10 @@
 if __name__ == "__main__":
     """Print addition of args"""
     import sys
+
+    count = sys.argv
     add = 0
-    for s in range(len(sys.argv) - 1):
+    for s in range(len(count) - 1):
         add += int(s)
     print("{:d}".format(add))
+    # print(count)
