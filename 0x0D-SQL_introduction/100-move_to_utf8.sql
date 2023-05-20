b4948810b942caacd8750f269c3847bca57672d7
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-05-20 21:36:15 +0100

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- converts 'hbtn_0c_0' database to 'utf8mb4' and collate 'utf8mb4_unicode_ci'
 USE hbtn_0c_0;
-ALTER hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACRTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
