c782ed8ee0728f28f9a9edf552fa06da38f3d58e
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-05-20 10:04:10 +0100

    Create 9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,4 @@
+-- creates a "second_table" in the database and add multiple rows
+CREATE TABLE IF NOT EXIXTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
