006cb0e2fa9017046f3867401491041170922d87
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-05-21 13:53:23 +0100

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities 
 (id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, 
-name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state_id);
+name VARCHAR(256) NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(id));
