aba1211affea40a6c7575f05fc44bb251ff1d20b
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-05-21 13:14:45 +0100

    Create 2-create_read_user.sql

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- create database 'hbtn_0d_2' and user "user_0d_2"
+-- user_0d_2 should only have 'select' privilege in the database 'hbtn_0d_2'
+-- if the database 'hbtn_0d_2' exists script should not fail
+-- if user "user_0d_2" exists script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
