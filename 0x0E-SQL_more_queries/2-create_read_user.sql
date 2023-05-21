a0e08ef3e0b9b3d12e1779abd78e3a81a044109f
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-05-21 13:21:00 +0100

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@
 -- if user "user_0d_2" exists script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' 
 IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
