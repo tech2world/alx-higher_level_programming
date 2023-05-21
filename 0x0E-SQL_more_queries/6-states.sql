4ee61d1ea32d03eb24111360be0b09686c47e19b
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-05-21 13:41:51 +0100

    Create 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- create database 'hbtn_0d_usa' & table 'states'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
