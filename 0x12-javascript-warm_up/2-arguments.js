a4b3da11aa042037832e026864e15837fe63701b
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-15 06:54:55 +0100

    Create 2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (Process.argv.length === 2) {
+    console.log('No argument');
+} else if (Process.argv.length === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Argument found');
+}
