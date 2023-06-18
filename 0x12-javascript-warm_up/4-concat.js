8f947aaa63780e0bb1e19f1d55ad95660af4477e
tech2world <abdulazeezibrahim847@gmail.com>
2023-06-18 22:07:28 +0100

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 if (!process.argv[2]) {
-  console.log('undefined');
+  process.argv[2] = 'undefined';
 } else if (!process.argv[3]) {
-  console.log('undefined');
+  process.argv[3] = 'undefined';
 }
 console.log(process.argv[2] + ' is ' + process.argv[3]);
