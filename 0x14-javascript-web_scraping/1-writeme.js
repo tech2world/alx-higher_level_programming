113b5300953650bc685b0284d59163340011e73a
tech2world <abdulazeezibrahim847@gmail.com>
2023-07-26 00:55:08 +0100

    write to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// get command line arg
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
