c7f0ce1694b71f12e1302b4043bcfe0801e52bbf
tech2world <abdulazeezibrahim847@gmail.com>
2023-07-26 01:49:03 +0100

    loripsum_api

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const fs = require('fs');
+const urlToRequest = process.argv[2];
+const filePath = process.argv[3];
+
+request.get({url: urlToRequest, encoding:'utf-8'}, (error, _response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+      
+    });
+  }
+});
