6863cd3ba28f85ee31c2191bc5229ef424057a63
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-08-05 22:10:38 +0100

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,7 +10,7 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      rect = '';
+      let rect = '';
       for (let j = 0; j < this.width; j++) {
         rect += 'X';
       }
